2ad7a3ed53b189f10710a59dc14a4cd8e9b21d4d
MelanieGEFFRE <melaniegeffre2@gmail.com>
2022-12-01 12:02:08 +0100

    day one

---
+++ project/day1.gd
@@ -0,0 +1,54 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$OptionButton.add_item("test", 0)
+	$OptionButton.add_item("puzzle", 1)
+	
+	$OptionButton.connect("item_selected", self, "_process_with_input")
+	
+	_process_with_input(0)
+	
+func _process_with_input(index):	
+	var input_file = File.new()	
+	input_file.open("res://input/1/"+$OptionButton.get_item_text(index)+".txt", File.READ)
+	
+	var maxCal = 0
+	var calList = []
+	var currentCal = 0
+	
+	for value in input_file.get_as_text().split("\n"):
+		if value == "":
+			if maxCal<currentCal:
+				maxCal=currentCal
+			calList.append(currentCal)
+			currentCal = 0
+		else:
+			currentCal += int(value)
+			
+	if maxCal<currentCal:
+				maxCal=currentCal
+	
+	calList.append(currentCal)
+	
+	calList.sort_custom(MyCustomSort, "sort_descending")
+	
+	$Label.text = str(maxCal)
+	$Label2.text = str(calList[0]+calList[1]+calList[2])
+	
+class MyCustomSort:
+	static func sort_descending(a, b): # Par premier élément croissant
+		if a > b:
+			return true
+		return false
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
